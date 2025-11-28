d4f45f983b9d6bb1cd0408765f91c59a5335e22b
UltralyticsAssistant <web@ultralytics.com>
2025-11-28 19:44:37 +0000

    Auto-format by https://ultralytics.com/actions

--- mypredict.py
+++ mypredict.py
@@ -2,8 +2,8 @@ from ultralytics import YOLO
 
 model = YOLO(r"yolo11n.pt")
 model.predict(
-    source=r"D:\Personal\Desktop\img\ac76c3cc7fcb813cd066f3cf3d4ea17c.mp4",   # 预测目标
+    source=r"D:\Personal\Desktop\img\ac76c3cc7fcb813cd066f3cf3d4ea17c.mp4",  # 预测目标
     # source = 0,  # 打开摄像头
-    save = False,                    # 保存预测结果
-    show = True,                   # 立刻显示结果
+    save=False,  # 保存预测结果
+    show=True,  # 立刻显示结果
 )
